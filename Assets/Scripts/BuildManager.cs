5bc2d49d2177bb1667fa58bd56558f288b120b6f
Liam Mentens <r0703354@student.thomasmore.be>
2020-02-18 20:24:16 +0100

    Added User Interface

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -21,9 +21,12 @@ public class BuildManager : MonoBehaviour
     public GameObject standardTurretPrefab;
     public GameObject missileLauncherPrefab;
 
+    public GameObject buildEffect;
+
     private TurretBlueprint turretToBuild;
 
     public bool CanBuild { get { return turretToBuild != null; } }
+    public bool HasMoney { get { return PlayerStats.Money >= turretToBuild.cost; } }
    
     public void BuildTurretOn(Node node)
     {
@@ -38,6 +41,9 @@ public class BuildManager : MonoBehaviour
         //Build turret
         GameObject turret = (GameObject)Instantiate(turretToBuild.prefab, node.GetBuildPosition(), Quaternion.identity);
         node.turret = turret;
+
+        GameObject effect = (GameObject)Instantiate(buildEffect, node.GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 5f);
 
         Debug.Log("Turret build! Money left:" + PlayerStats.Money);
     }
